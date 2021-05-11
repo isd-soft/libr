337ff2c5495c382c43735c341a13a6dc8cb2b181
AlexandruBaris <a.barisenschi@gmail.com>
2021-05-11 16:42:49 +0300

    start spring.io

---
+++ src/test/java/com/isd/libr/LibrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isd.libr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
