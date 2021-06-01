2c41dfa1d9412cacb79f46b2807b6fc22d685452
Kirill <grosukirill009@gmail.com>
2021-06-01 10:30:24 +0300

    Documented BookReactionService and CommentReactionService

--- src/main/java/com/isd/libr/service/BookReactionService.java
+++ src/main/java/com/isd/libr/service/BookReactionService.java
@@ -3,7 +3,32 @@ package com.isd.libr.service;
 import com.isd.libr.web.dto.requests.BookReactionRequest;
 import com.isd.libr.web.entity.ReactionType;
 
+/**
+ * BookReactionService is an interface for {@link com.isd.libr.web.entity.BookReaction} entity.
+ *
+ * <p>Preferred implementation {@code BookReactionServiceImpl}</p>
+ *
+ * @author Alexandru Baris
+ */
 public interface BookReactionService {
+    /**
+     * Creates an object of class {@link com.isd.libr.web.entity.BookReaction} from provided data and inserts into database.
+     * @param bookReactionRequest object of type {@link BookReactionRequest} containing fields: <ul>
+     *                            <li>User ID</li>
+     *                            <li>Book ID</li>
+     *                            <li>Reaction Type</li>
+     * </ul>
+     */
     void react(BookReactionRequest bookReactionRequest);
+
+    /**
+     * Counts all reactions present in database by type. Used for statistics purposes.
+     * @param type enum with one of fields: <ul>
+     *             <li>HEART</li>
+     *             <li>LAUGH</li>
+     *             <li>SAD</li>
+     * </ul>
+     * @return number of total reactions of provided type.
+     */
     Integer countReactionTypes(ReactionType type);
 }
--- src/main/java/com/isd/libr/service/CommentReactionService.java
+++ src/main/java/com/isd/libr/service/CommentReactionService.java
@@ -2,6 +2,22 @@ package com.isd.libr.service;
 
 import com.isd.libr.web.dto.requests.CommentReactionRequest;
 
+/**
+ * BookReactionService is an interface for {@link com.isd.libr.web.entity.CommentReaction} entity.
+ *
+ * <p>Preferred implementation {@code CommentReactionServiceImpl}</p>
+ *
+ * @author Grosu Kirill
+ */
 public interface CommentReactionService {
+    /**
+     * Creates an object of class {@link com.isd.libr.web.entity.CommentReaction} from provided data and inserts into database.
+     *
+     * @param commentReactionRequest object of type {@link CommentReactionRequest} containing fields: <ul>
+     *                               <li>User ID</li>
+     *                               <li>Comment ID</li>
+     *                               <li>Reaction Type</li>
+     *                               </ul>
+     */
     void react(CommentReactionRequest commentReactionRequest);
 }
